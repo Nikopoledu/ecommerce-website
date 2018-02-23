e348b952d669bd35ec5133fa0165469edfddf151
telukcoding <telukcoding@gmail.com>
2018-02-23 21:41:30 +0800

    Add files via upload

---
+++ cart_index.blade.php
@@ -0,0 +1,117 @@
+@extends('layouts.includes.main')
+@section('title','Products')
+@section('content')
+
+<div class="container"> 
+  <ol class="breadcrumb">
+  <li class="breadcrumb-item"><a href="#">Home</a></li>
+  <li class="breadcrumb-item"><a href="#">Library</a></li>
+  <li class="breadcrumb-item active">Data</li>
+</ol>
+<div class="row">
+<div class="col-md-8 ">
+  <div class="panel panel-default">
+    <div class="panel-heading">Edit Cart</div>
+      <div class="panel-body">
+<br>
+    <div class="row">
+     <div class="col-md-5">
+        <div class="thumnail-image">
+          <img src="{{asset('/images/product_image.jpg')}}" alt="" />
+           </div>
+        </div>
+    <div class="col-md-7">    
+        <div class="title-product"> <h4 style="margin-top: 0;"><b>DressBerry Women Navy Blue Solid Chambray A-Line Dress</b></h4></div>
+           <form action="#" method="post">
+                {{ csrf_field() }}
+                 {{ method_field('PUT') }}
+               <input name="image" type="hidden" value="">
+                  <div class="price"> <p> $80</p></div>
+    <div class="row">
+        <div class="col-xs-2">
+            <label>QTY :</label>
+        </div>
+        <div class="col-md-8">
+             <select  type="text" name="qty" class="form-control" >
+                    <option value="#" class="disable selected">1</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+              </select>
+        </div>
+    </div>
+    <br>
+    <div class="row"> 
+        <div class="col-md-2">
+            <label>SIZE :</label>
+        </div>
+          <div class="col-md-8">
+              <select name="size" class="form-control" >
+                    <option value="#" >S</option>
+                        <option value="S">S</option>
+                        <option value="M">M</option>
+                        <option value="L">L</option>
+                        <option value="XL">XL</option>
+              </select>
+        </div>
+    </div>
+    <br>
+    <div class="row">
+       <div class="col-md-4"></div>
+         <div class="col-md-3">
+            <button type="submit" class="btn btn-success btn-sm btn-block"><i class="fa fa-check"></i></button>
+         </div>
+       </form> 
+        <div class="col-md-3"> 
+            <form action="#" method="post">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                 <button type="submit" class="btn btn-danger btn-block btn-sm"><i class="fa fa-trash-o"></i></button>
+            </form>
+
+         </div>
+        </div>
+      </div>
+     </div> <!-- row foreach -->
+<hr>
+  </div> <!-- panel body -->
+       <div class="panel-footer">
+          <div class="row">
+            <div class="panel-footer">
+           </div>
+         <div class="col-md-7">
+             <a href="#" class="btn btn-success" style="background-color: #303734; border: none">Continue Shopping</a>
+              <a href="#" class="btn btn-success" style="background-color: #303734; border: none">Checkout Now</a>
+          </div>
+                     
+          <div class="col-md-5">
+             <div class="row">
+                 <div class="col-md-5">
+                     <b>
+                      Items: 23 <br>
+                      Tax: $23 <br> 
+                      </b>
+                </div>
+                  <div class="col-md-7">
+                      <b>
+                        Sub Total: $ 22 <br>
+                        Grand Total: $ 23</b>
+                  </div>
+            </div>
+          </div>
+         </div>
+      </div> <!-- panel footer -->       
+    </div>
+</div> <!-- col md 8 -->
+
+         <div class="col-md-4">
+           @include('layouts.includes.sidebar_right')
+        </div> <!-- col md 4 -->
+
+    </div>
+</div> <!-- container -->
+
+@endsection
---
+++ sidebar_right.blade.php
@@ -0,0 +1,38 @@
+    <div class="panel-group">
+      <div class="panel panel-default">
+  			<div class="panel-heading">Sidebar</div>
+         </div>
+          </div>
+
+     <div class="panel-group">
+        <div class="panel panel-default">
+  			   <div class="panel-body">
+ 	            <div class="alert alert-info" style=" margin-bottom: 0px;">
+ 	              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s 
+              </div>
+  			</div>
+      </div>
+     </div>
+
+     <!-- foreach -->
+      <div class="panel-group">
+        <div class="panel panel-default">
+         <div class="panel-body"> 
+            <div class="thumnail-image">
+              <img src="{{asset('/images/product_image.jpg')}}" alt="" />
+         </div>
+      <div class="info-product">
+          <button  data-toggle="modal" data-target="#">Info Product <i class="fa fa-info-circle"></i></button>
+      </div>
+          <div class="title-product"> <h4>DressBerry Women Blue Solid Chambray A-Line Dress</h4></div>
+            <div class="size"><p>X, M, L </p> </div>
+              <div class="price">  <p>$120</p></div>
+              <div class="addToCart">
+                  <i class="fa fa-heart-o"></i>
+                  <i class="fa fa-share"></i>
+                  <a href="#" class="btn btn-primary pull-right btn-sm">Add to Cart</a>
+              </div>
+             </div>
+          </div>
+      </div>
+        <!-- 3ndforeach -->
